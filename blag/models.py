aa27997bf4144089594d22531451451ee97d1028
Mohamed-Mamdouh <mohamedmamdohzaher@gmail.com>
2023-02-06 21:46:08 +0200

    design model blag & about

--- blag/models.py
+++ blag/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class post (models.Model):
+    titel = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post/')
+    puplish_date = models.DateTimeField(default=timezone.now )
+    content = models.TextField(max_length=10000)
+
+    def __str__(self):
+        return self.titel
+
+
--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class About (models.Model):
+    blag_name=models.CharField(max_length=100)
+    image = models.ImageField(upload_to='about')
+    subtitle = models.TextField(max_length=500)
+    li_in_link = models.URLField()
+    githup_link = models.URLField()
+    whats_app_link = models.URLField()
+    aboutme = models.TextField(max_length=10000)
+
+    
+
